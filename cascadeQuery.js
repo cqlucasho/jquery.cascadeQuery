387d041fc5516d9ed615beb81524716fa7196e40
lucashe <cqlucasho@gmail.com>
2014-11-03 10:27:04 +0800

    Update cascadeQuery.js

--- cascadeQuery.js
+++ cascadeQuery.js
@@ -1,25 +1,36 @@
 (function ($) {
-	var HtmlRender = function ($data, $opts) {
-		this.data = $data;
-		this.opts = $opts;
-
+	var HtmlRender = function () {
+		var $that = this;
+		
+		// 初始化数据
+		this.init = function ($data, $opts) {
+			$that.data = $data;
+			$that.opts = $opts;
+		};
+		
 		// 例如：城市区县级联查询
 		this.areaLinkQuery = function () {
 			// eval转换视情况而定，返回的是字符串JSON格式才需要转换。
-			var obj = eval('(' + this.data + ')');
+			var obj = eval('(' + $that.data + ')');
 			
-			var html = '<option>' + this.opts.title + '</option>';
+			var html = '<option>' + $that.opts.title + '</option>';
 			$.each(obj, function (index, town) {
 				html += '<option value="' + town.TownID + '">' + town.TownName + '</option>';
 			});
 
-			this.opts.fill.empty().append(html);
+			$that.opts.fill.empty().append(html);
 		}
 		
 		// 添加其它方法
 		this.xxx = function() {}
-	}
+	};
 
+	$(document).ready(function () {
+		$.HtmlRenderObj = new HtmlRender();
+	});
+})(window.jQuery);
+
+(function ($) {
 	$.fn.cascadeQuery = function ($options) {
 		var opts = $.extend({}, $.fn.cascadeQuery.defaults, $options);
 		
@@ -43,14 +54,14 @@
 				
 				if (!opts.crossDomain) {
 					$.post(opts.url, JSON.parse(values), function (data) {
-						var html = new HtmlRender(data, opts);
-						html.调用方法;
+						$.HtmlRenderObj.init(data, opts);
+						eval(opts.method);
 					});
 				}
 				else {
 					$.getJSON(opts.url, JSON.parse(values), function (data) {
-						var html = new HtmlRender(data, opts);
-						html.调用方法;
+						$.HtmlRenderObj.init(data, opts);
+						eval(opts.method);
 					});
 				}
 			}
@@ -66,6 +77,8 @@
 			url: '',
 			// 提交数据
 			data: null,
+			// 指定htmlRender的调用方法
+			method: '',
 			// 标题
 			title: '',
 			// 是否跨域请求
